a2988d098d118b33953a4522ed0ff65c35702b0b
Rxmi-sk <Rslimk3@gmail.com>
2023-08-04 17:35:53 -0400

    more changes

--- weatherApp.py
+++ weatherApp.py
@@ -28,7 +28,7 @@ def main():
     # Getting location information from user
     city = input("What city do you live in?\n--> ")
     latitude, longitude, country= get_user_location(city)   # Converting city name to latitude and longitude
-    api_link=f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min&timezone=GMT"
+    api_link=f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto"
     # Fetching weather data using user's latitude and longitude
     r = requests.get(api_link)
     status_code = r.status_code # Getting the API's status
@@ -36,15 +36,18 @@ def main():
     # Checking if the API is working
     if status_code == 200:
        data = r.json()  # Getting the API response as a JSON
+       print(city+", "+country)
        max_temps = data["daily"]["temperature_2m_max"]  # Getting max weekly temperatures from the 'daily' section
        todays_max = max_temps[0]    # Getting today's max temperature
        min_temps = data["daily"]["temperature_2m_min"]  # Getting min weekly temperatures from the 'daily' section
        todays_min = min_temps[0]    # Getting today's min temperature
-    #    sunrise = data["daily"]["sunrise"]
-    #    sunset = data["daily"]["sunset"]
+       city_sunrise = data["daily"]["sunrise"]
+       sunrise = city_sunrise[0] 
+       city_sunset = data["daily"]["sunset"]
+       sunset = city_sunset[0] 
 
      
-       print(f'High: {todays_max}°C\nLow: {todays_min}°C\nSunrise: {sunrise}\nSunset: {sunset}') # printing the high and low temps for the day
+       print(f'High: {todays_max}°C\nLow: {todays_min}°C\nSunrise: {sunrise[-5:]}\nSunset: {sunset[-5:]}') # printing the high and low temps for the day
 
     else:
         print('Failed to fetch metadata.') # prints if the API is not working
