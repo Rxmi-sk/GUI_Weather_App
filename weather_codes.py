39fbd9318af90a65ebd2dbc6f27198cbbbbe6e5d
Rayane Slimane-Kadi <114273333+rayane-slimane-kadi@users.noreply.github.com>
2023-08-04 20:46:58 -0400

    Create weather_codes.py

---
+++ weather_codes.py
@@ -0,0 +1,33 @@
+# Function that returns the forecast based on its code
+def decode(wno_code):
+    code_table={
+        "0": "Clear Sky",
+        "1": "Mainly Clear",
+        "2": "Partly Cloudy",
+        "3": "Overcast",
+        "45": "Fog",
+        "48": "Depositing Rime Fog",
+        "51": "Light Drizzle",
+        "53": "Moderate Drizzle",
+        "55": "Dense Intensity Drizzle",
+        "56": "Light Intensity Freezing Drizzle",
+        "57": "Dense Intensity Freezing Drizzle",
+        "61": "Light Intensity  Rain",
+        "63": "Moderate Intensity Rain",
+        "65": "Heavy Intesnity Rain",
+        "66": "Light Freezing Rain",
+        "67": "Heavy Intensity Freezing Rain",
+        "71": "Slight Snow Fall",
+        "73": "Moderate Snow Fall",
+        "75": "Heavy Snow Fall",
+        "77": "Snow Grains",
+        "80": "Slight Rain Showers",
+        "81": "Moderate Rain Showers",
+        "82": "Violent Rain Showers",
+        "85": "Slight Snow Showers",
+        "86": "Heavy Snow Showers",
+        "95": "Thunderstorm",
+        "96": "Thrunderstorm with Hail",
+        "99": "Thunderstrom with Heavy Hail",
+    }
+    return code_table[str(wno_code)]
